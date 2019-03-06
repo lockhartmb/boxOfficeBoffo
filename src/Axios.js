d58c649fd25071b4fe113c197a7da9df10ce05b9
Morgan <lockhart.mb@gmail.com>
2019-03-06 16:52:51 -0500

    hooking up the year select but need to get the axios call to change

--- src/Axios.js
+++ src/Axios.js
@@ -6,17 +6,18 @@ const apiKey = '9ce0982a9b86c5a78ad6ab14e214b652'
 
 // Axios component has a state of results that can be passed back up to GamePage as needed
 class Axios extends Component {
-	constructor(props) {
-		super(props);	
+	constructor() {
+		super();	
 		this.state = {
-			// results: []
+
 		}
 	}
+
 	
 	// used props to get the year from GamePage and saved as variable. Variable is used in search parameters to make that dynamic
 	// componentDidMount is an async function
 	async componentDidMount() {
-		const {currentYear} = this.props
+		let { handleYear } = this.props;
 
 			await axios({
 				method: 'GET',
@@ -31,8 +32,8 @@ class Axios extends Component {
 						api_key: apiKey,
 						format: 'json',
 						language: 'en-US',
-						'primary_release_date.gte': `${currentYear}-05-01`,
-						'primary_release_date.lte': `${currentYear}-09-04`,
+						'primary_release_date.gte': `${handleYear}-05-01`,
+						'primary_release_date.lte': `${handleYear}-09-04`,
 						region: 'US',
 						page: 1,
 						sort_by: 'revenue.desc'
--- src/GamePage.js
+++ src/GamePage.js
@@ -9,7 +9,7 @@ class GamePage extends Component {
         super();
         this.state = {
             year: 2019,
-            results: [],
+            results: []
         }
     }
 
@@ -17,27 +17,51 @@ class GamePage extends Component {
         this.setState({
             results: movieResult
         })
-        console.log(this.state.results);
+        // console.log(this.state.results);
+    }
+
+    handleYear = (event) => {
+        event.preventDefault();
+        this.setState({
+            year: event.target.value
+        })
+
     }
 
     render() {
         return (
             <section>
                 <h1>Box Office Boffo</h1>
-                <select><option></option></select>
+                <select onChange={this.handleYear}>
+                    <option value="2019" >2019</option>
+                    <option value="2018">2018</option>
+                    <option value="2017">2017</option>
+                    <option value="2016">2016</option>
+                    <option value="2015">2015</option>
+                    <option value="2014">2014</option>
+                    <option value="2013">2013</option>
+                    <option value="2012">2012</option>
+                    <option value="2011">2011</option>
+                    <option value="2010">2010</option>
+                    <option value="2009">2009</option>
+                    <option value="2008">2008</option>
+                </select>
                 {/* We pass the state of year to the Axios component to modify the search parameters */}
-                <Axios 
-                currentYear={this.state.year}
-                getResults={this.getResults}
+
+                <Axios
+                    currentYear={this.state.year}
+                    getResults={this.getResults}
+                    handleYear={this.handleYear}
                 />
+
                 <div className="movieCatalogue">
                     {this.state.results.map(movie => {
                         return (
                             <div key={movie.id} className="movieCatalogue__movie">
                                 {/* a link to a URL that doesn't exist yet, but when it does, it will be the ID of the movie I click on */}
-                                
-                                    <img src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={`Poster for ${movie.title}`} />
-                                
+
+                                <img src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={`Poster for ${movie.title}`} />
+
                             </div>
                         )
                     })
