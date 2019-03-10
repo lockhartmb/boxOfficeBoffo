e0c30dca9d666cbdded08b4c02bd62af1e5fbcc1
Morgan <lockhart.mb@gmail.com>
2019-03-09 19:23:53 -0500

    added code comments to CurrentList.js

--- src/CurrentList.js
+++ src/CurrentList.js
@@ -24,66 +24,49 @@ class CurrentList extends Component {
 		}));
 	};
 
+	// need to figure out how to delete only one movie from the list ie. .child() but for now it is deleting the whole list
 	handleDelete = (key) => {
 		console.log(key);
-
 		const dbRef = firebase.database().ref(this.props.userName);
-
 		dbRef.remove();
 	}
-
 
 	//it is for checking if the previous props is different from the new props, if it is the length are different it will reset the state
 	componentDidUpdate(prevProps) {
 		if (prevProps.chosenMovies.length !== this.props.chosenMovies.length) {
-
-
-			const titleList = [];
-
-
 			this.setState({
 				items: this.props.chosenMovies
 			})
-
 		}
 	}
 
+	// passing handleDelete function to child components
 	render() {
-
 		return <SortableList handleDelete={(key) => this.handleDelete(key)} items={this.state.items} onSortEnd={this.onSortEnd} />;
 	}
 }
 
-
-
 const SortableList = SortableContainer(({ items, handleDelete }) => {
-
 
 	return (
 		<ul className="currentList">
 			{items.map((value, index) => {
+				// passing handleDelete function and the firebase key to child components
 				return <SortableItem handleDelete={(key) => handleDelete(key)} firebaseKey={value.key} key={index} index={index} title={value.title} />
-
 			}
-
-
 			)}
-
 		</ul>
 	);
 });
 
-
+// the delete button here can now use the handleDelete function and the firebase key that have been passed down
 const SortableItem = SortableElement(({ title, firebaseKey, handleDelete }) => {
 	return (
 		<li id={firebaseKey} key={firebaseKey}>
 			{title}
 			<button onClick={() => handleDelete(firebaseKey)}>Delete</button>
-
 		</li>);
 });
-
-
 
 // render(<CurrentList />, document.getElementById('root'));
 export default CurrentList;
